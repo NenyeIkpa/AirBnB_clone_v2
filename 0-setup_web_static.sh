21f943c39ac53d00cb26854f973ed7202f66c8df
NenyeIkpa <nenyeikpa@gmail.com>
2024-01-11 20:36:36 +0000

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -30,16 +30,16 @@ sample_text="<html>
         <h1>Success! The devshowcase.tech server block is working!</h1>
     </body>
 </html>"
-if [ ! -f "/data/web_static/releases/test/index.html" ]; then
-	echo "$sample_text" > /data/web_static/releases/test/index.html
-fi
+echo "$sample_text" > /data/web_static/releases/test/index.html
 
 # Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
 # If the symbolic link already exists, it should be deleted and recreated every time the script is ran
-if [ -h "/data/web_static/current" ]; then
-	sudo rm /data/web_static/current
+# -s creates the link; -f unlinks existing symlink and recreates the link
+# sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+if [ -d "/data/web_static/current" ]; then
+	rm /data/web_static/current
 fi
-sudo ln -s /data/web_static/releases/test /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist).
 # This should be recursive; everything inside should be created/owned by this user/group.
@@ -47,16 +47,11 @@ sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 path_to_file="/etc/nginx/sites-available/default"
-string_to_find='server_name _;'
-string_to_append='\\n\tlocation /hbnb_static/ {\n\t\talias /var/www/devshowcase.tech/hbnb_static;\n\t}'
-# string_to_append='
-#    \tlocation /hbnb_static/ {
-#    \t\troot /var/www/devshowcase.tech;
-#    \t\talias /var/www/devshowcase.tech/hbnb_static;
-# \t}'
 if ! grep -q "location /hbnb_static/" "$path_to_file"; then
-	sed -i "/$string_to_find/a$string_to_append" /etc/nginx/sites-available/default
+	sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 fi
+
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 
 # restart Nginx after updating the configuration
 sudo service nginx restart
